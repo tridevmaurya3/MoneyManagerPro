92371c14cef308a9194e3efd45e8385b365e33c7
tridevmaurya3 <tridevmaurya3@gmail.com>
2026-08-02 20:03:47 +0530

    Register database migration 11 to 12

--- app/src/main/java/com/example/moneymanagerpro/database/DatabaseClient.java
+++ app/src/main/java/com/example/moneymanagerpro/database/DatabaseClient.java
@@ -25,7 +25,8 @@ public class DatabaseClient {
                 AppDatabase.MIGRATION_7_8,
                 AppDatabase.MIGRATION_8_9,
                 AppDatabase.MIGRATION_9_10,
-                AppDatabase.MIGRATION_10_11
+                AppDatabase.MIGRATION_10_11,
+                AppDatabase.MIGRATION_11_12
         ).build();
     }
 
