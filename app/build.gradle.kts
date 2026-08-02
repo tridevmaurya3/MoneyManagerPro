c165e74f60a222c5000445f1ea9bd7862c97246a
tridevmaurya3 <tridevmaurya3@gmail.com>
2026-08-02 16:49:22 +0530

    Bump release version to 1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.example.moneymanagerpro"
         minSdk = 24
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.8"
+        versionCode = 10
+        versionName = "1.9"
 
         testInstrumentationRunner =
             "androidx.test.runner.AndroidJUnitRunner"
