80dfb8ea54841f5ecb9d9e98b2ec7d352017e753
tridevmaurya3 <tridevmaurya3@gmail.com>
2026-08-02 18:12:53 +0530

    Bump app version for direct SMS access

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.example.moneymanagerpro"
         minSdk = 24
         targetSdk = 34
-        versionCode = 12
-        versionName = "2.1"
+        versionCode = 13
+        versionName = "2.2"
 
         testInstrumentationRunner =
             "androidx.test.runner.AndroidJUnitRunner"
