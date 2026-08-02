61606e48e2d45d398a1dbd2135256ba0788f3706
tridevmaurya3 <tridevmaurya3@gmail.com>
2026-08-02 18:55:34 +0530

    Bump release version for account synchronization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.example.moneymanagerpro"
         minSdk = 24
         targetSdk = 34
-        versionCode = 13
-        versionName = "2.2"
+        versionCode = 14
+        versionName = "2.3"
 
         testInstrumentationRunner =
             "androidx.test.runner.AndroidJUnitRunner"
